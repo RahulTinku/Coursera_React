b5fa5cd32014338bc040a089f087239eb8bee5fb
Rahul K (433154) <rahul.kumar-8@alstomgroup.com>
2022-01-15 19:53:10 +0530

    Assignment 2

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent';
 
 class Main extends Component {
   constructor(props) {
@@ -59,6 +60,11 @@ class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route path="/menu/:dishId" component={DishWithId} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
