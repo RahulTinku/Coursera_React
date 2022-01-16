7203fc220a4a55539c6c2d80465c3b861c7ada63
Rahul K (433154) <rahul.kumar-8@alstomgroup.com>
2022-01-16 22:18:53 +0530

    assignement 3

--- src/component/DishDetailsComponent.js
+++ src/component/DishDetailsComponent.js
@@ -1,91 +1,207 @@
-import React, { Component } from 'react';
+import { React, Component } from 'react';
 import {
   Card,
-  CardBody,
   CardImg,
   CardText,
+  CardBody,
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Row,
+  Col,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Form,
+  FormGroup,
+  Input,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const RenderDish = ({ dish }) => {
-  if (dish != null) {
-    return (
-      <div className="column col-12 col-md-5 m-1">
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
-};
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-const RenderComments = ({ comments }) => {
-  if (comments != null) {
-    const cmnts = comments.map((comment) => {
-      return (
-        <li key={comment.id}>
-          <p>{comment.comment}</p>
-          <p>
-            -- {comment.author}, &nbsp;
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+function RenderComments({ comments }) {
+  const allComments = comments.map((comment) => {
+    return (
+      <li>
+        <p>{comment.comment}</p>
+        <p>
+          -- {comment.author},{' '}
+          <span className="date timeago">
             {new Intl.DateTimeFormat('en-US', {
-              year: 'numeric',
-              month: 'long',
+              month: 'short',
               day: '2-digit',
+              year: 'numeric',
             }).format(new Date(comment.date))}
-          </p>
-        </li>
-      );
-    });
+          </span>
+        </p>
+      </li>
+    );
+  });
 
+  return (
+    <div>
+      <h4>Comments</h4>
+      <ul className="list-unstyled">{allComments}</ul>
+      <CommentForm />
+    </div>
+  );
+}
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  render() {
     return (
-      <div className="col-12 col-md-5 m-1">
-        <h4> Comments </h4>
-        <ul className="list-unstyled">{cmnts}</ul>
+      <div className="container">
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+
+          <ModalBody>
+            <LocalForm onSubmit={this.handleLogin}>
+              <Row md={{ size: 6, offset: 2 }}>
+                <div className="form-check">
+                  <Label>
+                    <strong>Rating</strong>
+                  </Label>
+                </div>
+              </Row>
+              <Row md={{ size: 3, offset: 1 }} className="mr-1 ml-1">
+                <Control.select
+                  model=".contactType"
+                  name="contactType"
+                  className="form-control"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </Control.select>
+              </Row>
+
+              <Row md={{ size: 6, offset: 2 }}>
+                <div className="form-check">
+                  <Label>
+                    <strong>Your Name</strong>
+                  </Label>
+                </div>
+              </Row>
+              <Row md={10} className="mr-1 ml-1">
+                <Control.text
+                  model=".firstname"
+                  id="firstname"
+                  name="firstname"
+                  placeholder="Your name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".firstname"
+                  show="touched"
+                  messages={{
+                    required: 'Required',
+                    minLength: 'Must be greater than 2 characters',
+                    maxLength: 'Must be 15 characters or less',
+                  }}
+                />
+              </Row>
+              <Label htmlFor="message" md={2}>
+                <strong>comment</strong>
+              </Label>
+              <Row className="form-group mr-1 ml-1">
+                <Control.textarea
+                  model=".message"
+                  id="message"
+                  name="message"
+                  rows="6"
+                  className="form-control"
+                />
+              </Row>
+              <Button type="submit" value="submit" color="primary">
+                Submit
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
       </div>
     );
-  } else {
-    return <div></div>;
   }
-};
-const DishDetail = (props) => {
-  const dish = props.dish;
+}
 
-  if (dish == null) {
-    return <div></div>;
-  } else {
+function RenderDish({ dish }) {
+  if (dish != null)
     return (
       <div className="container">
         <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/home">Home</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <Link to="/menu">Menu</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="row">
-          <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
         </div>
       </div>
     );
-  }
+  else return <div></div>;
+}
+
+const DishDetail = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish} />
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <RenderComments comments={props.comments} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DishDetail;
