6c845d8690798b561a286bec4cb827d0da916357
Rahul Kumar <r0k067s@homeoffice.wal-mart.com>
2024-01-06 01:36:04 +0530

    useContext complete

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,3 +1,20 @@
-export const ThemeProvider = ({ children }) => children;
+import {createContext, useContext, useState} from 'react';
 
-export const useTheme = () => ({ theme: "light" });
+// export const ThemeProvider = ({ children }) => children;
+
+//export const useTheme = () => ({ theme: "light" });
+
+const ThemeContext = createContext(undefined)
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState('dark');
+    return <ThemeContext.Provider 
+                value={{
+                    theme, 
+                    toggleTheme : () => setTheme(theme === 'light' ? 'dark' : 'light')
+                }}>
+                    {children}
+            </ThemeContext.Provider>
+}
+
+export const useTheme = ()=> useContext(ThemeContext)
