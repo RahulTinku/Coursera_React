eabb534b795f8d2586dd81691ccedc1a52337a69
Rahul Kumar <r0k067s@homeoffice.wal-mart.com>
2024-01-05 23:18:57 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,103 +1,85 @@
 import "./App.css";
-import { useState } from "react";
-import { validateEmail } from "./utils";
+import { ThemeProvider, useTheme } from "./ThemeContext";
+import Switch from "./Switch";
 
-const PasswordErrorMessage = () => {
+const Title = ({ children }) => {
+  const { theme } = useTheme();
   return (
-    <p className="FieldError">Password should have at least 8 characters</p>
+    <h2
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </h2>
+  );
+};
+
+const Paragraph = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <p
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </p>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Paragraph>
+        We are a pizza loving family. And for years, I searched and searched and
+        searched for the perfect pizza dough recipe. I tried dozens, or more.
+        And while some were good, none of them were that recipe that would
+        make me stop trying all of the others.
+      </Paragraph>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <Title>Little Lemon 🍕</Title>
+      <Switch />
+    </header>
+  );
+};
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
+    </div>
   );
 };
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState({
-    value: "",
-    isTouched: false,
-  });
-  const [role, setRole] = useState("role");
-
-  const getIsFormValid = () => {
-    if(firstName && validateEmail(email) && password.value.length >= 8 && (role === "individual" || role === "business")){
-      return true;
-    }
-    return false;
-  };
-
-  const clearForm = () => {
-    // Implement this function
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPassword({
-      value: "",
-      isTouched: false,
-    });
-    setRole("role");
-  };
-
-  const handleSubmit = (event) => {
-   event.preventDefault();
-   clearForm();
-   alert("form is submitted successfully");
-   
-  }
+  const { theme } = useTheme();
   return (
-    <div className="App">
-     <form onSubmit={handleSubmit}>
-        <fieldset>
-          <h2>Sign up</h2>
-          <div className="Field">
-            <label>
-              First Name <sup>*</sup>
-            </label>
-            <input placeholder="First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value)}/>
-          </div>
-          <div className="Field">
-            <label>
-              Last Name
-            </label>
-            <input placeholder="Last Name" value={lastName} onChange={(e)=> setLastName(e.target.value)} />
-          </div>
-          <div className="Field">
-            <label>
-              Email address <sup>*</sup>
-            </label>
-            <input placeholder="Email address" value={email} onChange={(e)=> setEmail(e.target.value)} />
-          </div>
-          <div className="Field">
-            <label>
-              Password <sup>*</sup>
-            </label>
-            <input 
-              placeholder="Password" 
-              type="password" 
-              value={password.value} 
-              onChange={(e)=>  setPassword({...password, value :e.target.value})}
-              onBlur={() => { 
-                setPassword({ ...password, isTouched: true }); 
-              }} 
-              />
-            {password.isTouched && password.value.length < 8 ? PasswordErrorMessage() : null}
-          </div>
-          <div className="Field">
-            <label>
-              Role <sup>*</sup> 
-            </label>
-            <select onChange={(e)=> setRole(e.target.value)} value={role}>
-              <option value="role">Role</option>
-              <option value="individual">Individual</option>
-              <option value="business">Business</option>
-              </select>
-          </div>
-          <button type="submit" disabled={!getIsFormValid()}>
-            Create account
-          </button>
-        </fieldset>
-     </form>
+    <div
+      className="App"
+      style={{
+        backgroundColor: theme === "light" ? "white" : "black",
+      }}
+    >
+      <Header />
+      <Page />
     </div>
   );
 }
 
-export default App;
+function Root() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export default Root;
