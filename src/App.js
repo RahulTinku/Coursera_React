3b2a2fa65076fec3fe845990af083fc3e8dd8394
Rahul Kumar <rahulkumar271@gmail.com>
2024-01-06 01:40:27 +0530

    test

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const Content = () => {
         We are a pizza loving family. And for years, I searched and searched and
         searched for the perfect pizza dough recipe. I tried dozens, or more.
         And while some were good, none of them were that recipe that would
-        make me stop trying all of the others.
+        make me stop trying all of the others. Add this for commit.
       </Paragraph>
     </div>
   );
